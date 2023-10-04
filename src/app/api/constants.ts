0b17b923fc3ac5b091facd8675fdddc8643b8de9
Marlon Angeli <marlonangeli91@gmail.com>
2023-10-04 14:57:13 -0300

    fix: remove timestamp field in API_QUERY_PARAMETERS

--- src/app/api/constants.ts
+++ src/app/api/constants.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   "https://www.duolingo.com/2017-06-30/users?username=";
 export const API_QUERY_PARAMETERS =
-  "&fields=users%7Bcourses,creationDate,currentCourse,currentCourseId,gemsConfig,globalAmbassadorStatus,hasPlus,id,lingots,name,picture,privacySettings,roles,streak,streakData%7BcurrentStreak,previousStreak%7D,totalXp,username%7D&_=1694487301124";
+  "&fields=users%7Bcourses,creationDate,currentCourse,currentCourseId,gemsConfig,globalAmbassadorStatus,hasPlus,id,lingots,name,picture,privacySettings,roles,streak,streakData%7BcurrentStreak,previousStreak%7D,totalXp,username";
